dfd8802f0dde9b9762a8cf0b3e3c67bf53219478
Norbert Billa <norbert.billa@gmail.com>
2025-08-08 13:33:46 +0200

    refactor: cache folder

--- sn_inference/src/cache/k_v_cache/k_v_cache.rs
+++ sn_inference/src/cache/k_v_cache/k_v_cache.rs
@@ -4,7 +4,9 @@ use mlx_rs::ops::indexing::{IndexMutOp, IndexOp};
 use mlx_rs::ops::{concatenate_axis, zeros_dtype};
 use std::sync::{Arc, RwLock};
 use mlx_rs::transforms::{async_eval, eval};
+use serde::{Deserialize, Serialize};
 use tracing::error;
+use sn_core::utils::rw_lock::RwLockExt;
 use crate::error::Error;
 use crate::utils::mlx::mlx_compute_lock::MLX_COMPUTE_LOCK;
 
@@ -53,59 +55,6 @@ impl KVCache {
               )
         }
        (Array::from_int(0), Array::from_int(0))
-    }
-
-    #[allow(non_snake_case)]
-    pub fn update_and_fetch_old(
-        &mut self,
-        keys: &Array,
-        values: &Array,
-    ) -> Result<(Array, Array), Exception> {
-        let prev = self.offset;
-        let shape = keys.shape();
-        let new_seq_len = shape[2];
-        let needs_resize = match &self.keys {
-            Some(cached_keys) => (prev + keys.shape()[2]) > cached_keys.shape()[2],
-            None => true,
-        };
-
-        if self.keys.is_none() || needs_resize {
-            let prev = self.offset;
-            let B = shape[0];
-            let n_kv_heads = shape[1];
-            let k_head_dim = shape[3];
-            let v_head_dim = values.shape()[3];
-            let n_steps = (self.step + shape[2] - 1) / self.step;
-            let k_shape = [B, n_kv_heads, n_steps * self.step, k_head_dim];
-            let v_shape = [B, n_kv_heads, n_steps * self.step, v_head_dim];
-            let new_k = zeros_dtype(&k_shape, keys.dtype())?;
-            let new_v = zeros_dtype(&v_shape, values.dtype())?;
-            if let (Some(old_k), Some(old_v)) = (&self.keys.take(), &self.values.take()) {
-                if prev % self.step != 0 {
-                    self.keys = Some(old_k.index((.., 0..prev, ..)).clone());
-                    self.values = Some(old_v.index((.., 0..prev, ..)).clone());
-                }
-                self.keys = Some(concatenate_axis(&[old_k, &new_k], 2)?);
-                self.values = Some(concatenate_axis(&[old_v, &new_v], 2)?);
-            } else {
-                self.keys = Some(new_k);
-                self.values = Some(new_v);
-            }
-        }
-
-        self.offset += new_seq_len;
-        // Write new keys and values into the cache
-        let keys_cache = self.keys.as_mut().unwrap();
-        let values_cache = self.values.as_mut().unwrap();
-
-        let start = prev;
-        let end = start + new_seq_len;
-
-        keys_cache.index_mut((.., .., start..end, ..), keys);
-        values_cache.index_mut((.., .., start..end, ..), values);
-        let keys_out = keys_cache.index((.., .., 0..self.offset, ..));
-        let values_out = values_cache.index((.., .., 0..self.offset, ..));
-        Ok((keys_out, values_out))
     }
 
     #[allow(non_snake_case)]
@@ -182,6 +131,14 @@ impl KVCache {
 
         Ok((keys_out, values_out))
     }
+
+    pub fn cache_size(&self) -> usize {
+        if let (Some(keys), Some(value)) = (&self.keys, &self.values) {
+            keys.nbytes() + value.nbytes()
+        } else {
+            0
+        }
+    }
 }
 
 unsafe impl Send for KVCache {}
@@ -234,4 +191,16 @@ fn test_kv_cache_update_and_fetch() {
 
     // Sanity: Final offset should match total seq_len
     assert_eq!(cache.offset, 2 * seq_len + big_seq);
+}
+
+pub trait CacheSize {
+    fn cache_size(&self) -> usize;
+}
+
+impl CacheSize for Arc<RwLock<Vec<ArcCacheItem>>> {
+    fn cache_size(&self) -> usize {
+        self.read_lock("read_cache_size").map_or(0, |cache| {
+            cache.iter().map(|item| item.read_lock("read_item_size").map_or(0, |c| c.cache_size())).sum()
+        })
+    }
 }
